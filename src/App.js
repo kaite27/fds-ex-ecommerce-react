3cb3623ed031395b48d4dd0223a9b304cb080101
Kim Hyemin <beigenut@gmail.com>
2018-06-22 15:56:07 +0900

    Add redandant div for testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
-// admin, user, user2 => 0
-
 import LoginPage from './pages/LoginPage';
 
 export default class App extends Component {
   render() {
-    return <LoginPage />;
+    return (
+      <React.Fragment>
+        <div>TEST!Push</div>
+        <LoginPage />
+      </React.Fragment>
+    );
   }
 }
