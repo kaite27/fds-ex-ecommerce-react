6e9a89eb49fa111b38d05253431ce68298c02c4b
Kim Hyemin <beigenut@gmail.com>
2018-06-24 22:51:54 +0900

    Add NewProductCT on MainPage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import NavBarCT from '../containers/NavBarCT';
+import NewProductCT from '../containers/NewProductCT';
 
 export default class MainPage extends Component {
   render() {
     return (
       <React.Fragment>
         <NavBarCT />
+        <NewProductCT />
       </React.Fragment>
     );
   }
