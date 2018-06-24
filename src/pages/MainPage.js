daf36818aa80c46bf55a7aabac43fe5eaec5ae24
Kim Hyemin <beigenut@gmail.com>
2018-06-24 19:08:22 +0900

    Add Main Page including NavBarCT container

---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import NavBarCT from '../containers/NavBarCT';
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBarCT />
+      </React.Fragment>
+    );
+  }
+}
