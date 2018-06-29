960eca99029833727dd4a418ddfed6116d9223dd
Kim Hyemin <beigenut@gmail.com>
2018-06-29 18:24:11 +0900

    Apply props on ProductItem.js

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -3,11 +3,13 @@ import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function ProductItem({
-  id, // comment's id on json server
-  Colors,
-  Sizes,
+  id, // to use as a product id
+  colors,
+  sizes,
   title,
   desc,
+  category,
+  accSoldCnt,
   unitPrice,
   marketPrice,
   imageURL,
@@ -15,36 +17,40 @@ export default function ProductItem({
   return (
     <div className="column is-one-third product-page__li">
       <div className="product-page__box card">
-        <Link className="link-to-product-detail" to={`/product/${id}`} />
         <div className="img-box">
-          <img
-            className="product-page__img--main"
-            src="images/slide-1.jpg"
-            alt="New Item 01"
-          />
-          <Link to="/" className="img-box__btn card-link btn btn-success">
-            <i className="icon-wish far fa-heart" /> Wish
+          <Link className="link-to-product-detail" to={`/product/${id}`}>
+            <img
+              className="product-page__img--main"
+              src={imageURL}
+              alt={title}
+            />
+            <Link to={`/`} className="img-box__btn card-link btn btn-success">
+              <i className="icon-wish far fa-heart" /> Wish
+            </Link>
           </Link>
         </div>
         <div className="product-page__card">
-          <p className="product-page__title" />
-          <p className="product-page__desc" />
+          <p className="product-page__title">{title}</p>
+          <p className="product-page__desc">{desc}</p>
         </div>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">
-            <span className="list-group-item__unitPrice" />
+            <span className="list-group-item__unitPrice">{unitPrice}</span>
             <strong>
-              <span className="list-group-item__marketPrice" />
+              <span className="list-group-item__marketPrice">
+                {marketPrice}
+              </span>
             </strong>
           </li>
           <li className="list-group-item">
             <p>Options :</p>
             <div className="divider" />
             <p className="list-group-item__variants">
-              Colors : <span className="list-group-item__color">Red</span>
+              Colors :{' '}
+              <span className="list-group-item__color">{colors + ''}</span>
             </p>
             <p className="list-group-item__variants">
-              Size : <span className="list-group-item__size">10</span>
+              Size : <span className="list-group-item__size">{sizes + ''}</span>
             </p>
           </li>
         </ul>
