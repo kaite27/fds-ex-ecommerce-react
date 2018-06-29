30b82b1c4d919bd1c02cd683215c23aa002e93c3
Kim Hyemin <beigenut@gmail.com>
2018-06-29 12:22:28 +0900

    Add ProductPage to put a Link to using url control

---
+++ src/pages/ProductPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import ProductsCT from '../containers/ProductsCT';
+
+export default class ProductPage extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ProductsCT />
+      </React.Fragment>
+    );
+  }
+}
