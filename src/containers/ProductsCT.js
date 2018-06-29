5fc06106a418025ae874f81f258680ea0febe461
Kim Hyemin <beigenut@gmail.com>
2018-06-29 18:27:25 +0900

    Inherit props to show each data and to link to pages on ProductList

--- src/containers/ProductsCT.js
+++ src/containers/ProductsCT.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import NavBar from '../components/NavBar';
 import ProductList from '../components/ProductList';
+import { ProductProvider, ProductConsumer } from '../contexts/ProductContext';
 
 export default class ProductsCT extends React.Component {
   state = {
@@ -11,7 +13,19 @@ export default class ProductsCT extends React.Component {
     return (
       <React.Fragment>
         <NavBar />
-        <ProductList />
+        <ProductProvider>
+          <ProductConsumer>
+            {({ products }) => (
+              <ProductList
+                as={Link}
+                products={products.map(p => ({
+                  linkProps: { to: `/product/${p.id}` },
+                  ...p,
+                }))}
+              />
+            )}
+          </ProductConsumer>
+        </ProductProvider>
       </React.Fragment>
     );
   }
