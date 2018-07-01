8587bdc504c963eb3ecd1c339814cdfdf725acf9
Kim Hyemin <beigenut@gmail.com>
2018-07-01 19:14:46 +0900

    Add functions; priceCalculate, updateSize and updateColor to calculate prices and update selected color and size to reload info

--- src/contexts/DetailProductContext.js
+++ src/contexts/DetailProductContext.js
@@ -5,10 +5,6 @@ import mallAPI from '../mallAPI';
 const { Provider, Consumer } = React.createContext();
 
 class DetailProductProvider extends React.Component {
-  // static defaultProps = {
-  //   id: null, // 초기 id
-  // };
-
   state = {
     colors: [],
     sizes: [],
@@ -26,10 +22,18 @@ class DetailProductProvider extends React.Component {
     // avgRate: '',
     // cntRates: 0,
     loading: false,
+    prices: [],
+    subTotal: 0,
+    total: 0,
+    salesTax: 0,
   };
 
   // reviews = [];
   async componentDidMount() {
+    await this.loadProduct();
+  }
+
+  loadProduct = async e => {
     const { id } = this.props;
     this.setState({ loading: true });
     try {
@@ -39,7 +43,7 @@ class DetailProductProvider extends React.Component {
       // const reviewRes = await mallAPI.get('/reviews?productId=${id}');
       const attrRes = await mallAPI.get(`/attributes?productId=${id}`);
 
-      // 중복 제거
+      // 컬러와 사이즈 중복 값 제거
       const avoidColor = attrRes.data
         .map(p => p.color)
         .filter(function(item, i, arr) {
@@ -50,11 +54,12 @@ class DetailProductProvider extends React.Component {
         .map(p => p.size)
         .filter(function(item, i, arr) {
           return i === arr.indexOf(item);
-        });
+        })
+        .sort((prev, current) => prev - current);
 
       this.setState({
         colors: avoidColor,
-        sizes: avoidSize.sort((prev, current) => prev - current),
+        sizes: avoidSize,
         productId: res.data.map(p => p.productId),
         productTitle: res.data.map(p => p.product.productTitle),
         productDesc: res.data.map(p => p.product.productDesc),
@@ -70,11 +75,66 @@ class DetailProductProvider extends React.Component {
     } finally {
       this.setState({ loading: false });
     }
-  }
+  };
+
+  updateColor = async color => {
+    const { id } = this.props;
+    this.setState({ loading: true });
+    try {
+      const res = await mallAPI.get(
+        `/attributes?productId=${id}&color=${color}&size=${this.state.size}`
+      );
+      this.setState({
+        color: color,
+        quantity: res.data.map(p => p.quantity),
+        attrSKU: res.data.map(p => p.attrSKU),
+        productMarketPrice: res.data.map(p => p.productMarketPrice),
+      });
+      this.priceCalculate(1);
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  updateSize = async size => {
+    const { id } = this.props;
+    this.setState({ loading: true });
+    try {
+      const res = await mallAPI.get(
+        `/attributes?productId=${id}&size=${size}&color=${this.state.color}`
+      );
+      this.setState({
+        size: size,
+        quantity: res.data.map(p => (p.quantity ? p.quantity : 0)),
+        attrSKU: res.data.map(p => p.attrSKU),
+        productMarketPrice: res.data.map(
+          p => (p.productMarketPrice ? p.productMarketPrice : 0)
+        ),
+        productUnitPrice: res.data.map(p => p.productUnitPrice),
+      });
+      this.priceCalculate(1);
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  priceCalculate = inputValue => {
+    const subTotal = this.state.productMarketPrice * parseInt(inputValue, 10);
+    const salesTax = subTotal * 0.06625;
+    const total = subTotal + salesTax;
+    this.setState({
+      subTotal: subTotal.toFixed(2),
+      salesTax: salesTax.toFixed(2),
+      total: total.toFixed(2),
+    });
+  };
 
   render() {
     const value = {
       ...this.state,
+      onUpdateSize: this.updateSize,
+      onUpdateColor: this.updateColor,
+      onPriceCalculate: this.priceCalculate,
     };
     return <Provider value={value}>{this.props.children}</Provider>;
   }
