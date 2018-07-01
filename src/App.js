ea3a89f922e45ddce7a7a31faa00aea8af8ec67b
Kim Hyemin <beigenut@gmail.com>
2018-07-01 12:19:29 +0900

    Delete Redirect which is not necssary for now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
