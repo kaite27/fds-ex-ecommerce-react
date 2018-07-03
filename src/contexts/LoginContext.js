9c2bc5ca8fb972b23853e85066805aa2df0b3d92
DongbeenSeo <puus1021@gmail.com>
2018-07-03 17:39:06 +0900

    edit LoginForm, LoginContext, LoginFormCT, CSS of LoginForm

--- src/contexts/LoginContext.js
+++ src/contexts/LoginContext.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
+
 import mallAPI from '../mallAPI.js';
 
 const { Provider, Consumer } = React.createContext();
 
 class LoginProvider extends Component {
+  state = {
+    success: false,
+  };
   login = async (username, password) => {
-    try {
-      const payload = {
-        username: username,
-        password: password,
-      };
-      const res = await mallAPI.post('/users/login', payload);
+    if (username === '' || password === '') {
+      alert('please input ID or Password');
+    } else {
+      try {
+        const payload = {
+          username: username,
+          password: password,
+        };
+        const res = await mallAPI.post('/users/login', payload);
 
-      localStorage.setItem('token', res.data.token);
-      localStorage.setItem('username', username);
+        localStorage.setItem('token', res.data.token);
+        localStorage.setItem('username', username);
 
-      const UserRes = await mallAPI.get('/me');
-      localStorage.setItem('userId', UserRes.data.id);
-    } catch (e) {
-      if (e.response && e.response.status === 400) {
-        alert('Wrong ID or Password!');
+        const UserRes = await mallAPI.get('/me');
+        localStorage.setItem('userId', UserRes.data.id);
+        this.setState({ success: true });
+      } catch (e) {
+        if (e.response && e.response.status === 400) {
+          alert('Invalid username and password!');
+          this.setState({ success: false });
+        } else {
+          alert('Network error. Please try again.');
+        }
       }
-      throw e;
     }
   };
 
@@ -35,7 +46,9 @@ class LoginProvider extends Component {
     const value = {
       login: this.login,
       logout: this.logout,
+      success: this.state.success,
     };
+
     return <Provider value={value}>{this.props.children}</Provider>;
   }
 }
