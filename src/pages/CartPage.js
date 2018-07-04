8dd48e5853315fbd86b0a39fe42efc3e886c710e
Kim Hyemin <beigenut@gmail.com>
2018-07-04 17:47:58 +0900

    Add import NavBarCT and use

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import CartCT from '../containers/CartCT';
+import NavBarCT from '../containers/NavBarCT';
 
 export default class CartPage extends Component {
   render() {
     return (
       <React.Fragment>
+        <NavBarCT />
         <CartCT />
       </React.Fragment>
     );
