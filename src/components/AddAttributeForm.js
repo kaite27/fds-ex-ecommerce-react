be04ee605bb3de49d74116c7bea3ab02dd453d5a
Kim Hyemin <beigenut@gmail.com>
2018-07-05 14:36:13 +0900

    Change class to className

--- src/components/AddAttributeForm.js
+++ src/components/AddAttributeForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class AddAttributeForm extends Component {
   state = {
+    attCount: 1,
     attributes: [
       {
         attrSKU: '',
@@ -63,55 +64,51 @@ export default class AddAttributeForm extends Component {
     this.setState({ attributes: newattributes });
   };
 
-  // submit
-  handleSubmit = evt => {
-    const { name, attributes } = this.state;
-    alert(`Incorporated: ${name} with ${attributes.length} attributes`);
-  };
-
   handleAddAttribute = () => {
     this.setState({
       attributes: this.state.attributes.concat([
         {
           attrSKU: '',
           color: '',
-          size: 0,
-          productUnitPrice: 0,
-          productMarketPrice: 0,
-          quantity: 0,
+          size: '',
+          productUnitPrice: '',
+          productMarketPrice: '',
+          quantity: '',
         },
       ]),
+      attCount: this.state.attCount + 1,
     });
   };
 
   handleRemoveAttribute = idx => () => {
     this.setState({
       attributes: this.state.attributes.filter((s, sidx) => idx !== sidx),
+      attCount: this.state.attCount - 1,
     });
   };
 
   render() {
     return (
-      <div class="add-variants">
-        <p class="add-subtitle title is-3">Product Variants</p>
-        <form class="form-add-product__send">
-          <div class="form-container">
-            <div class="column-labels attr-labels">
-              <label class="attr-sku">SKU #</label>
-              <label class="attr-color">Color</label>
-              <label class="attr-size">Size</label>
-              <label class="attr-unit-price">Unit Price</label>
-              <label class="attr-market-price">Market Price</label>
-              <label class="attr-quantity">Quantity</label>
+      <div className="add-variants">
+        <p className="add-subtitle title is-3">Product Variants</p>
+        <form className="form-add-product__send">
+          <div className="form-container">
+            <div className="column-labels attr-labels">
+              <label className="attr-sku">SKU #</label>
+              <label className="attr-color">Color</label>
+              <label className="attr-size">Size</label>
+              <label className="attr-unit-price">Unit Price</label>
+              <label className="attr-market-price">Market Price</label>
+              <label className="attr-quantity">Quantity</label>
             </div>
             {this.state.attributes.map((attribute, idx) => (
-              <div class="product product-attr">
-                <div class=" attr-sku">
+              <div className="product product-attr">
+                <div className=" attr-sku">
                   <input
                     type="text"
                     value={attribute.attrSKU}
                     onChange={this.handleAttributeChangeAttrSKU(idx)}
-                    class="input attr-input attr-sku__input"
+                    className="input attr-input attr-sku__input"
                     placeholder="SKU number"
                     required
                     data-toggle="tooltip"
@@ -119,12 +116,12 @@ export default class AddAttributeForm extends Component {
                     title="ex) SKU-22a-0211"
                   />
                 </div>
-                <div class=" attr-color">
+                <div className=" attr-color">
                   <input
                     type="text"
                     value={attribute.color}
                     onChange={this.handleAttributeChangeColor(idx)}
-                    class="input attr-input  attr-sku__color"
+                    className="input attr-input  attr-sku__color"
                     placeholder="One of colours"
                     required
                     data-toggle="tooltip"
@@ -132,12 +129,12 @@ export default class AddAttributeForm extends Component {
                     title="ex) Red, Yellow .."
                   />
                 </div>
-                <div class=" attr-size">
+                <div className=" attr-size">
                   <input
                     type="text"
                     value={attribute.size}
                     onChange={this.handleAttributeChangeSize(idx)}
-                    class="input attr-input  attr-sku__size"
+                    className="input attr-input  attr-sku__size"
                     placeholder="One of sizes"
                     required
                     data-toggle="tooltip"
@@ -145,38 +142,42 @@ export default class AddAttributeForm extends Component {
                     title="ex) 90, Small, US-1 .."
                   />
                 </div>
-                <div class=" attr-unit-price">
+                <div className=" attr-unit-price">
                   <input
                     type="number"
                     value={attribute.productUnitPrice}
                     onChange={this.handleAttributeChangeUPrice(idx)}
-                    class="input attr-input  attr-sku__unit-price"
+                    className="input attr-input  attr-sku__unit-price"
                     placeholder="Unit price"
+                    step="0.01"
+                    data-number-to-fixed="2"
                     required
                     data-toggle="tooltip"
                     data-placement="bottom"
                     title="important! only number available! ex) 90.99"
                   />
                 </div>
-                <div class=" attr-market-price">
+                <div className=" attr-market-price">
                   <input
                     type="number"
                     value={attribute.productMarketPrice}
                     onChange={this.handleAttributeChangeMPrice(idx)}
-                    class="input attr-input  attr-sku__market-price"
+                    className="input attr-input  attr-sku__market-price"
                     placeholder="Market price"
                     required
+                    step="0.01"
+                    data-number-to-fixed="2"
                     data-toggle="tooltip"
                     data-placement="bottom"
                     title="important! only number available! ex) 90.99"
                   />
                 </div>
-                <div class=" attr-quantity">
+                <div className=" attr-quantity">
                   <input
                     type="number"
                     value={attribute.quantity}
                     onChange={this.handleAttributeChangeQuantity(idx)}
-                    class="input attr-input  attr-sku__quantity"
+                    className="input attr-input  attr-sku__quantity"
                     placeholder="Quantity"
                     required
                     data-toggle="tooltip"
@@ -192,19 +193,22 @@ export default class AddAttributeForm extends Component {
                 </button>
               </div>
             ))}
-            <div class="row-buttons">
-              <div class="field is-grouped">
-                <div class="control">
+            <div className="row-buttons">
+              <div className="field is-grouped">
+                <div className="control">
                   <button
                     type="button"
                     onClick={this.handleAddAttribute}
-                    class="button is-info add-reset-btn"
+                    className="button is-info add-reset-btn"
                   >
                     + Add more variant
                   </button>
                 </div>
-                <div class="control">
-                  <button type="button" class="button is-success add-reset-btn">
+                <div className="control">
+                  <button
+                    type="submit"
+                    className="button is-success add-reset-btn"
+                  >
                     Publish product
                   </button>
                 </div>
