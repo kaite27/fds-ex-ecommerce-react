fe16be67bd7e49ef45c444b63d4d8d6c757e1fac
Kim Hyemin <beigenut@gmail.com>
2018-07-06 15:59:53 +0900

    Remove LoadingBox which is not necessary

--- src/components/Subscribes.js
+++ src/components/Subscribes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import SubscribesForm from './SubscribesForm';
-import LoadingBox from '../components/LoadingBox';
 import {
   SubscribesProvider,
   SubscribesConsumer,
