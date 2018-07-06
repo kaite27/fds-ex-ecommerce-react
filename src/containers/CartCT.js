a84c3833d4e709904b1b578998a44f8a9267097e
Kim Hyemin <beigenut@gmail.com>
2018-07-06 16:00:27 +0900

    Give loading state as a prop to CartList

--- src/containers/CartCT.js
+++ src/containers/CartCT.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { CartProvider, CartConsumer } from '../contexts/CartContext';
 import CartList from '../components/CartList';
-import LoadingBox from '../components/LoadingBox';
+
 export default class CartCT extends React.Component {
   render() {
     return (
@@ -19,27 +19,24 @@ export default class CartCT extends React.Component {
             deleteCartItem,
             loading,
           }) =>
-            loading ? (
-              <LoadingBox />
-            ) : (
-              <CartList
-                id={id}
-                as={Link}
-                total={total}
-                salesTax={salesTax}
-                subTotal={subTotal}
-                deleteCartItem={async id => {
-                  await deleteCartItem(id);
-                }}
-                updateSelectedQtt={async (quantity, id) => {
-                  await updateSelectedQtt(quantity, id);
-                }}
-                carts={carts.map(c => ({
-                  linkProps: { to: `/product/${c.productId}` },
-                  ...c,
-                }))}
-              />
-            )
+            <CartList
+              id={id}
+              as={Link}
+              loading={loading}
+              total={total}
+              salesTax={salesTax}
+              subTotal={subTotal}
+              deleteCartItem={async id => {
+                await deleteCartItem(id);
+              }}
+              updateSelectedQtt={async (quantity, id) => {
+                await updateSelectedQtt(quantity, id);
+              }}
+              carts={carts.map(c => ({
+                linkProps: { to: `/product/${c.productId}` },
+                ...c,
+              }))}
+            />
           }
         </CartConsumer>
       </CartProvider>
