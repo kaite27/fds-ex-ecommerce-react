5a21f72eafdc85d298c699e46dbe675e4653a1cd
beigenut <beigenut@gmail.com>
2019-07-08 15:54:51 -0400

    Needs to get onCategory context from DetailProduct

--- src/components/DetailProductBreadcrumb.js
+++ src/components/DetailProductBreadcrumb.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function DetailProductBreadcrumb({ category, productId }) {
+export default function DetailProductBreadcrumb({
+  onCategory,
+  category,
+  productId,
+}) {
   return (
     <React.Fragment>
       <div className="rows">
         <nav className="breadcrumb" aria-label="breadcrumbs">
           <ul>
             <li className="breadcrumb-home">
-              <Link to="#">Home</Link>
+              <Link to="/">Home</Link>
             </li>
             <li className="breadcrumb-product">
-              <Link to="#">Products</Link>
+              <Link to="/product">Products</Link>
             </li>
-            <li className="breadcrumb-category">
-              <Link to="#">{category}</Link>
+            <li
+              className="breadcrumb-category"
+              onClick={category => onCategory(category)}
+            >
+              <Link>{category}</Link>
             </li>
             <li className="is-active breadcrumb-sku">
-              <Link to="#">{productId}</Link>
+              <Link>{productId}</Link>
             </li>
           </ul>
         </nav>
