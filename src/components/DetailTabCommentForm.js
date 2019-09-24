eed8c58cebfd2555107e4626f355c7b6e1f1b6b7
Beigenut <beigenut@gmail.com>
2019-09-24 19:13:32 -0400

    Redirect to login page

--- src/components/DetailTabCommentForm.js
+++ src/components/DetailTabCommentForm.js
@@ -2,65 +2,66 @@ import React, { Component } from 'react';
 import { Button, Form, FormGroup, FormControl } from 'react-bootstrap';
 
 export default class DetailTabCommentForm extends Component {
-  state = {
-    newCommentBody: '',
-  };
+    state = {
+        newCommentBody: '',
+    };
 
-  handleInputChange = e => {
-    this.setState({
-      newCommentBody: e.target.value,
-    });
-  };
+    handleInputChange = e => {
+        this.setState({
+            newCommentBody: e.target.value,
+        });
+    };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const { onComment } = this.props;
-    if (localStorage.getItem('token')) {
-      onComment(this.ratingRef.value, this.state.newCommentBody);
-    } else {
-      alert('Need to Login');
-    }
-    this.setState({
-      newCommentBody: '',
-    });
-  };
+    handleSubmit = e => {
+        e.preventDefault();
+        const { onComment } = this.props;
+        if (localStorage.getItem('token')) {
+            onComment(this.ratingRef.value, this.state.newCommentBody);
+        } else {
+            alert('To leave a review, you need to Login first');
+            window.location.href = "/login"
+        }
+        this.setState({
+            newCommentBody: '',
+        });
+    };
 
-  render() {
-    const { newCommentBody } = this.state;
-    return (
-      <div className="new-reviwe-box">
-        <Form inline onSubmit={this.handleSubmit}>
-          <FormGroup controlId="formControlsSelect">
-            <FormControl
-              componentClass="select"
-              placeholder="select"
-              className="review-lable-rate"
-              inputRef={ref => {
-                this.ratingRef = ref;
-              }}
-            >
-              <option value="★ ★ ★ ★ ★">★ ★ ★ ★ ★</option>
-              <option value="★ ★ ★ ★">★ ★ ★ ★</option>
-              <option value="★ ★ ★">★ ★ ★</option>
-              <option value="★ ★">★ ★</option>
-              <option value="★">★</option>
-            </FormControl>
-          </FormGroup>
-          <FormGroup controlId="formInlineComment">
-            <FormControl
-              componentClass="input"
-              className="review-input-comment"
-              type="text"
-              placeholder="Your comment.."
-              value={newCommentBody}
-              onChange={this.handleInputChange}
-            />
-          </FormGroup>{' '}
-          <Button type="submit" bsStyle="primary">
-            Leave Comment
+    render() {
+        const { newCommentBody } = this.state;
+        return (
+            <div className="new-reviwe-box">
+                <Form inline onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="formControlsSelect">
+                        <FormControl
+                            componentClass="select"
+                            placeholder="select"
+                            className="review-lable-rate"
+                            inputRef={ref => {
+                                this.ratingRef = ref;
+                            }}
+                        >
+                            <option value="★ ★ ★ ★ ★">★ ★ ★ ★ ★</option>
+                            <option value="★ ★ ★ ★">★ ★ ★ ★</option>
+                            <option value="★ ★ ★">★ ★ ★</option>
+                            <option value="★ ★">★ ★</option>
+                            <option value="★">★</option>
+                        </FormControl>
+                    </FormGroup>
+                    <FormGroup controlId="formInlineComment">
+                        <FormControl
+                            componentClass="input"
+                            className="review-input-comment"
+                            type="text"
+                            placeholder="Your comment.."
+                            value={newCommentBody}
+                            onChange={this.handleInputChange}
+                        />
+                    </FormGroup>{' '}
+                    <Button type="submit" bsStyle="primary">
+                        Leave Comment
           </Button>
-        </Form>
-      </div>
-    );
-  }
+                </Form>
+            </div>
+        );
+    }
 }
